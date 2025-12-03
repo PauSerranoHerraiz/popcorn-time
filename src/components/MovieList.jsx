3d6e3fa880820df1b4b130464a046cf559df9032
Pau Serrano <pauserrano14@gmail.com>
2025-12-03 12:17:59 +0100

    feat: display movie images

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,12 +4,15 @@ function MovieList (){
 
     return (
         <>
-        <h1>This is the MovieList</h1>
+        <h2>List of movies</h2>
 
         {movies.map((moviesObj)=> {
             return (
                 <div key={moviesObj.id} className ="card">
                 <p>{moviesObj.title}</p>
+
+                <img src={moviesObj.imgURL} alt="Movie poster" />
+
                 <p>{moviesObj.year}</p>
                 <p>{moviesObj.rating}</p>
                 </div>
