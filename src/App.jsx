84224f0b39ed65a193e6d89d64e8e326a4379f3f
Pau Serrano <pauserrano14@gmail.com>
2025-12-04 12:20:09 +0100

    refactor: lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react"
+import movies from "./data/movies.json"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Movielist from "./components/MovieList"
 
 
 function App() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
   
+  const deleteMovie = (movieId) => {
+    const newList = moviesToDisplay.filter((movieDetails) => {
+      if (movieDetails.id !== movieId) {
+        return true;
+      }
+      else {
+        return false
+      }
+
+    })
+
+    setMoviesToDisplay(newList)
+  }
+
+
   return (
     <>
 
-      <Header />
-      <Movielist />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <Movielist moviesArr={moviesToDisplay} onDelete={deleteMovie} />
       <Footer />
-     
+
     </>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,36 +1,17 @@
-
-import { useState } from "react"
-import movies from "../data/movies.json"
 import MovieSummary from "./MovieSummary"
 
-function MovieList() {
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
-
-    const deleteMovie = (movieId) => {
-        const newList = moviesToDisplay.filter((movieDetails) => {
-            if (movieDetails.id !== movieId) {
-                return true;
-            }
-            else {
-                return false
-            }
-
-        })
-
-        setMoviesToDisplay(newList)
-    }
+function MovieList(props) {
 
     return (
         <>
-            <h2>Number of movies: {moviesToDisplay.length}</h2>
+            {/*}<h2>Number of movies: {moviesToDisplay.length}</h2> {*/}
 
-            {moviesToDisplay.map((moviesObj) => {
+            {props.moviesArr.map((moviesObj) => {
                 return (
 
                     <MovieSummary key={moviesObj.id}
                         movieDetails={moviesObj}
-                        onDelete={deleteMovie}
+                        onDelete={props.onDelete}
                     />
                 )
 
